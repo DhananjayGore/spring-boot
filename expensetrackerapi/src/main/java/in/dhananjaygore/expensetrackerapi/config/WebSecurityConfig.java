8b6ebabf897882bfaf0d12e3b90edf6bd5cb93ae
DhananjayGore <dvgore1997@gmail.com>
2022-06-26 07:25:26 +0530

    webSecurity annotation added

--- expensetrackerapi/src/main/java/in/dhananjaygore/expensetrackerapi/config/WebSecurityConfig.java
+++ expensetrackerapi/src/main/java/in/dhananjaygore/expensetrackerapi/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -22,6 +23,7 @@ import in.dhananjaygore.expensetrackerapi.security.JwtRequestFilter;
 
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	
